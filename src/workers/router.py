675b2f7b62a68b2e27d86bd31d34124f20f99a8a
ibnabii <someone@somewhere.net>
2024-03-10 20:51:33 +0100

    Got Worker schemas working (finally!!!)

--- src/workers/router.py
+++ src/workers/router.py
@@ -1,18 +1,20 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 from src.workers.models import Worker
+from src.workers.schemas import WorkerCreate, WorkerRead
 
 router = APIRouter()
 
 
-@router.get("/", description="All workers, for admin only")
-async def get_all_workers() -> List[Worker]:
-    return await Worker.find_all().to_list()
+@router.get("/", description="All workers, for admin only", response_model=list[WorkerRead])
+async def get_all_workers() -> List[WorkerRead]:
+    workers = await Worker.find_all().to_list()
+    return [WorkerRead.from_orm(worker) for worker in workers]
 
 
-@router.post("/", description="Create a new Worker", status_code=201)
-async def create_worker(worker: Worker) -> dict:
-    new_worker = await Worker.create(worker)
+@router.post("/", description="Create a new Worker", status_code=status.HTTP_201_CREATED)
+async def create_worker(worker: WorkerCreate) -> dict:
+    new_worker = await Worker.create(Worker(**worker.dict()))
     return {"id": new_worker.id}
---
+++ src/workers/schemas.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+
+class BaseWorker(BaseModel):
+    name: str = Field(max_length=100)
+    version: str = Field(max_length=100)
+    pid: int | None = None
+
+    class Config:
+        from_attributes = True
+
+
+class WorkerCreate(BaseWorker):
+    pass
+
+
+class WorkerRead(BaseWorker):
+    id: UUID
+    created: datetime
+    updated: datetime
+    valid_until: datetime
+    # revision_id: UUID | None = Field(None)
+
